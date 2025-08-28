14c3e98c408798a1dcabf63f2526b73a1fb51428
PHILIP THOMAS <thomasphil6889@gmail.com>
2025-08-28 23:59:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 document.getElementById('contact-form').addEventListener('submit', function (e) {
   e.preventDefault();
   emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', this, 'YOUR_PUBLIC_KEY')
